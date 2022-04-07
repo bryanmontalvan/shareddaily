2decf4cf6c236dea5cc69f345c30a770363aeb90
bryanmontalvan <68972382+bryanmontalvan@users.noreply.github.com>
2022-04-07 09:00:30 -0400

    Added built-in mantine notification Added `NotificationsProvider` and using the `showNotifcation` hook where I can remove the more verbose Notification item

--- pages/index.js
+++ pages/index.js
@@ -2,12 +2,15 @@ import CarForm from '../lib/CarForm.js'
 import SearchForm from '../lib/SearchForm.js'
 
 import { MantineProvider } from '@mantine/core';
+import { NotificationsProvider } from '@mantine/notifications';
 
 export default function Home() {
   return (
-    <MantineProvider theme={{ fontFamily: 'Open Sans'}}>
+    <MantineProvider theme={{ fontFamily: 'Open Sans' }}>
+      <NotificationsProvider>
         <CarForm />
         <SearchForm />
+      </NotificationsProvider>
     </MantineProvider>
   )
 }
