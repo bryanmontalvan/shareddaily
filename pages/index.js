ce78714cd5a29ebe3d5272efb865e1b4aa1fcc12
bryanmontalvan <68972382+bryanmontalvan@users.noreply.github.com>
2022-04-21 16:30:52 -0400

    OM-Variable changes and footer additon - Made all OM-methods the same - Added placeholder to search field - Added footer to page

--- pages/index.js
+++ pages/index.js
@@ -3,13 +3,27 @@ import SearchForm from '../lib/SearchForm.js'
 
 import { MantineProvider } from '@mantine/core';
 import { NotificationsProvider } from '@mantine/notifications';
+import { Footer } from '../lib/Footer.js';
+
+// Links for footer
+const footerlinks = [
+  {
+    "link": 'https://github.com/bryanmontalvan/shareddaily',
+    "label": 'GitHub'
+  },
+  {
+    "link": 'https://mantine.dev/',
+    "label": 'Created using Mantine'
+  }
+]
 
 export default function Home() {
   return (
-    <MantineProvider theme={{ fontFamily: 'Open Sans' }}>
-      <NotificationsProvider>
+    <MantineProvider theme={{ fontFamily: 'Open Sans'}}>
+      <NotificationsProvider style={{height: '100vh'}}>
         <CarForm />
         <SearchForm />
+        <Footer links={footerlinks} />
       </NotificationsProvider>
     </MantineProvider>
   )
